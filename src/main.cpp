88f8a472178567840087c598c888b41ea7626f83
Tim van Osch <timvosch@pollex.nl>
2020-07-01 19:21:57 +0200

    include samd21 & cmsis atpack

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,9 @@
-#include <mbed.h>
-// #include <LowPowerTimer.h>
+#include <sam.h>
 
-DigitalOut led(PA06);
-// DigitalOut loraSW(PA17);
-// Serial lora(PA18, PA19, 9600);
-// Serial serial(PA10, PA11, 115200);
-Timeout t;
+int main()
+{
 
-
-void isr1() {
-  led != led;
-}
-
-int main() {
-  t.attach(&isr1, 5);
-  while(1) {
-    deepsleep();
+  while (1)
+  {
   }
 }
