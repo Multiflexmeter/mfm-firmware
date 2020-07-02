8e07ba37f2f397c784286e16f299d2be765985a5
Tim van Osch <timvosch@pollex.nl>
2020-07-02 18:46:33 +0200

    Blink

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,14 @@
 
 int main()
 {
+  PORT->Group[0].DIRSET.reg |= PORT_PA06;
 
   while (1)
   {
+    PORT->Group[0].OUTTGL.reg |= PORT_PA06;
+    for (int x = 0; x < 30000; x++)
+    {
+      __ASM("nop");
+    }
   }
 }
