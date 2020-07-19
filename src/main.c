c2e6de99f7baa39ae8e128c5d73db7a4d01ee402
Tim van Osch <timvosch@pollex.nl>
2020-07-19 17:55:51 +0200

    Use delay function in main

--- src/main.c
+++ src/main.c
@@ -1,27 +1,22 @@
 #include "main.h"
 
-void delay()
+void delay(unsigned long timeMS)
 {
-  for (int i = 0; i < 100000; i++)
-  {
-    asm("nop");
-  }
+  unsigned long till = millis() + timeMS;
+  while (millis() < till)
+    ;
 }
 
-unsigned long lastTick = 0;
 int main(void)
 {
   init_system_clock();
 
   pinMode(LED, OUTPUT);
-  char state = 0;
   while (1)
   {
-    if (millis() - lastTick >= 1000)
-    {
-      state = !state;
-      pinWrite(LED, state);
-      lastTick = millis();
-    }
+    pinWrite(LED, HIGH);
+    delay(1000);
+    pinWrite(LED, LOW);
+    delay(1000);
   }
 }
